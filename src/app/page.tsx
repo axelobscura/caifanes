3c26cc29f00b93e648563f3a24c74e4f0a1e5b2d
axelinstrumental <aobscura@instrumental.net>
2024-06-12 15:36:49 -0600

    font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="font-Rajdhani bg-blue-td text-6xl">CAIFANES</h1>
+    <main className="flex min-h-screen flex-col bg-hero-image items-center justify-center p-24">
+      <h1 className="font-Rajdhani bg-blue-td text-8xl font-bold">CAIFANES</h1>
     </main>
   );
 }
